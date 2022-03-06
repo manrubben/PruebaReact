ec43a76b6a98fd89d987526d2f3d653facb2e897
Manu <manrubben@alum.us.es>
2022-03-06 19:09:17 +0100

    List method and create method done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Axios from "axios";
 
 function App() {
+
+    const [movieName, setMovieName] = useState("");
+    const [review, setReview] = useState("");
+    const [movieReviewList, setMovieList] = useState([]);
+
+    useEffect(() => {
+        Axios.get("http://localhost:3001/api/get").then((response) => {
+            setMovieList(response.data)
+        });
+    }, []);
+
+  const submitReview = () => {
+      Axios.post("http://localhost:3001/api/insert", {
+          movieName: movieName,
+          movieReview: review,
+      }).then(() => {
+          alert("successful insert");
+      });
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>CRUD APPLICATION</h1>
+
+        <div className="form">
+            <label>Movie Name:</label>
+            <input type="text"
+                   name="movieName"
+                   onChange={(event) => {
+                       setMovieName(event.target.value);
+                   }}
+            />
+
+            <label>Movie Review:</label>
+            <input type="text"
+                   name="movieReview"
+                   onChange={(e) => {
+                       setReview(e.target.value);
+                   }}
+            />
+
+            <button onClick={submitReview}>Submit</button>
+
+            {movieReviewList.map((val) => {
+                return <h1>Movie name: {val.movieName} | Movie review: {val.movieReview}</h1>
+            })}
+        </div>
     </div>
   );
 }
